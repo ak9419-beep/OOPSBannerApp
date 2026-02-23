17946bd78b1777ef5e0a1d57d5b4b9fbaefc489f
Arjit <arjitkumarmishra52@gmail.com>
2026-02-23 11:08:48 +0530

    WIP: local changes to OOPSBannerApp

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,44 @@
-public class OOPSBannerApp{
+public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        // Letter patterns stored as String arrays
+        String[] O = {
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+
+        String[] P = {
+            "****** ",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      "
+        };
+
+        String[] S = {
+            " ***** ",
+            "*      ",
+            " ***** ",
+            "      *",
+            " ***** "
+        };
+
+        // Store letters in order: O O P S
+        String[][] banner = { O, O, P, S };
+
+        // Number of rows in each letter
+        int rows = O.length;
+
+        // Print banner using loops
+        for (int i = 0; i < rows; i++) {
+            for (String[] letter : banner) {
+                System.out.print(letter[i] + "  ");
+            }
+            System.out.println();
+        }
     }
 }
